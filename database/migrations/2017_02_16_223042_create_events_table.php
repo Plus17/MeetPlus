137b4059cf57447a36eaaf7a0c66d7a97806d44d
plus17 <serge@openmailbox.org>
2017-02-16 17:05:06 -0600

    migration and model for events

---
+++ database/migrations/2017_02_16_223042_create_events_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+          $table->increments('id');
+          $table->string('name');
+          $table->date('start');
+          $table->date('end');
+          $table->string('place');
+          $table->string('longitude')->nullable();
+          $table->string('latitude')->nullable();
+          $table->mediumtext('description');
+          $table->enum('status', ['active', 'finished', 'canceled'])->default('active');
+          $table->integer('category_id')->unsigned();
+          $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+          $table->integer('user_id')->unsigned();
+          $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+          $table->timestamps();
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('events');
+    }
+}
