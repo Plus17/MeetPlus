b09b3193bf4f06777a03f0dc435b296171c1ee3c
plus17 <serge@openmailbox.org>
2017-02-16 17:06:40 -0600

    seeders for categories, users and events

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        Model::unguard();
+
+        DB::table('users')->insert([
+            'name' => 'Plus',
+            'email' => 'admin@email.com',
+            'role' => 'admin',
+            'password' => bcrypt('secret')
+        ]);
+        $this->call(UserTableSeeder::class);
+        $this->call(CategoryTableSeeder::class);
+        $this->call(EventTableSeeder::class);
+
+        Model::reguard();
     }
 }
