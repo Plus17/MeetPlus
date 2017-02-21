0ec441d68b0d0e5eba54cd9cbf2e394b77e9331a
plus17 <serge@openmailbox.org>
2017-02-21 13:54:00 -0600

    layout for dashboard

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,3 +14,11 @@ const { mix } = require('laravel-mix');
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css')
    .sass('resources/assets/sass/estilos.scss', 'public/css');
+
+mix.copy('resources/assets/sass/dashboard/sb-admin.css', 'public/css/dashboard/sb-admin.css')
+   .copy('resources/assets/sass/dashboard/plugins/morris.css', 'public/css/plugins/morris.css')
+   .copy('resources/assets/sass/font-awesome/css/font-awesome.min.css', 'public/css/font-awesome/css/font-awesome.min.css');
+
+mix.copy('resources/assets/js/plugins/morris/raphael.min.js', 'public/js/plugins/morris/raphael.min.js')
+   .copy('resources/assets/js/plugins/morris/morris.min.js', 'public/js/plugins/morris/morris.min.js')
+   .copy('resources/assets/js/plugins/morris/morris-data.js', 'public/js/plugins/morris/morris-data.js');
